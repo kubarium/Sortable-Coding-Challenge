e626e3396facf9a4f4c08cefd7673621a0f275c1
kubarium <iam@denizkumsal.com>
2016-09-06 14:04:10 -0400

    no message

--- index.py
+++ index.py
@@ -4,15 +4,15 @@ def result():
     with open("products.txt") as products, open("listings.txt") as listings, open("results.txt","w") as results:
 
         products = [json.loads(product) for product in products.readlines()]
-        listings = [json.loads(listing) for listing in listings.readlines()]
+        listings = [(json.loads(listing),str(listing)) for listing in listings.readlines()]
 
         for product in products:
 
             matches = list()
 
             for listing in listings:
-                if (product["manufacturer"] == listing["manufacturer"]) and ("family" in product and product["family"] in listing["title"]) and (product["model"] in listing["title"]):
-                    matches.append(listing)
+                if (product["manufacturer"] == listing[0]["manufacturer"]) and ("family" in product and product["family"] in listing[0]["title"]) and (product["model"] in listing[0]["title"]):
+                    matches.append(listing[1])
 
 
             results.write("%s\n" % json.JSONEncoder().encode({"product_name":product["product_name"], "listings":matches}))
