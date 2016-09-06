0894ddef2fa4a1e9d28274ebc828a80927cf3b66
Kumsal Obuz <iam@denizkumsal.com>
2016-09-06 01:41:27 -0400

    no message

--- index.py
+++ index.py
@@ -1,30 +1,20 @@
 import json
 
 def result():
-
-
-    with open("products.txt") as products, open("listings.txt") as listings:
-
-        results = {}
+    with open("products.txt") as products, open("listings.txt") as listings, open("results.txt","w") as results:
 
         products = [json.loads(product) for product in products.readlines()]
         listings = [json.loads(listing) for listing in listings.readlines()]
 
         for product in products:
-            print(product["product_name"])
 
-        print(count("family", products))
-        print(count("manufacturer", products))
-        print(count("model", products))
+            matches = list()
 
+            for listing in listings:
+                if (product["manufacturer"] == listing["manufacturer"]) and ("family" in product and product["family"] in listing["title"]) and (product["model"] in listing["title"]):
+                    matches.append(listing)
 
-
-def count(key, products):
-    occurrence = 0
-    for product in products:
-        if key in product:
-            occurrence+=1
-    return occurrence
+            results.write("%s\n" % str({"product_name":product["product_name"], "listings":matches}))
 
 if __name__ == "__main__":
     result()
