2ab13539d82db414a5f20b4c1010f79e92ec2c34
Kumsal Obuz <iam@denizkumsal.com>
2016-09-06 19:41:15 -0400

    no message

--- index.py
+++ index.py
@@ -2,9 +2,13 @@ import json
 from collections import OrderedDict
 
 def result():
-    with open("products.txt") as products, open("listings.txt") as listings, open("results.txt","w") as results:
-
-
+    with open("products.txt") as products, open("listings.txt", encoding="utf-8") as listings, open("results.txt","w") as results:
+        '''
+        for listing in listings:
+            if "Digital IXUS 310 HS - 1/2.3 ?MOS" in listing:
+                results.write("%s\n" % listing)
+        exit()
+        '''
         products = [json.loads(product) for product in products.readlines()]
         #listings = [(json.loads(listing)["title"],json.loads(listing)["manufacturer"], json.loads(listing, object_pairs_hook=collections.OrderedDict)) for listing in listings.readlines()]
         listings = [json.loads(listing, object_pairs_hook=OrderedDict) for listing in listings.readlines()]
@@ -20,7 +24,7 @@ def result():
 
             #json.dump(str({"product_name":product["product_name"], "listings":matches})+"\n", results)
 
-            results.write("%s\n" % json.JSONEncoder().encode({"product_name":product["product_name"],"listings":matches}))
+            results.write("%s\n" % json.JSONEncoder(ensure_ascii=False).encode({"product_name":product["product_name"],"listings":matches}))
 
 if __name__ == "__main__":
     result()
